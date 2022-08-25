1b30fefd147ed95113fd86c0a1b858f2ef225aad
Abhishek wani <75797884+Abhishek001-1@users.noreply.github.com>
2022-08-25 23:20:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,133 +1,151 @@
+
 from tkinter import *
 from tkinter import ttk
-from bubble_sort import bubble_sort
-from quick_sort import quick_sort
+import time
 import random
+from merge_sort import merge_sort
 
 root = Tk()
-root.title("Sorting Algorithm Visualizer")
-root.geometry('900x600')
-root.config(bg='black')
-data = []
+root.title("Sorting Algorithms Visualizer")
+root.maxsize(1400, 900)
+root.config(bg="black")
+
+algo_name = StringVar()
+algo_list = ['Merge Sort', 'Selection Sort', 'Bubble Sort', 'Insertion Sort']
+
+speed_name = StringVar()
+speed_list = ['Fast', 'Medium', 'Slow']
+
+arr = []
 
 
-# Drawing bars
-def drawdata(data, color_array):
+# This function is used for creating vertical bars according to the value present in the array
+def displayArr(arr, colorArray):
     canvas.delete("all")
-    canvas_height = 450
-    canvas_width = 870
-    x_width = canvas_width / (len(data) + 1)
-    offset = 10
-    spacing_bet_rect = 10
+    canvas_width = 800
+    canvas_height = 400
+    width_x = canvas_width / (len(arr) + 1)
+    ini = 4
+    space = 2
+    tempArr = [i / max(arr) for i in arr]
 
-    normalized_data = [i / max(data) for i in data]
-
-    for i, height in enumerate(normalized_data):
-        x1 = i * x_width + offset + spacing_bet_rect
-        y1 = canvas_height - height * 400
-        # We've multiplied 400 because we'll normalize our values with one formula so that our data
-        # won't exceed our canvas
-
-        x2 = (i + 1) * x_width
+    for i in range(len(tempArr)):
+        x1 = i * width_x + ini + space
+        y1 = canvas_height - tempArr[i] * 390
+        x2 = (i + 1) * width_x + ini
         y2 = canvas_height
-
-        canvas.create_rectangle(x1, y1, x2, y2, fill=color_array[i])
-        canvas.create_text(x1 + 2, y1, anchor=SW, text=str(data[i]), font=("new roman", 15, "italic bold"),
-                           fill="orange")
+        canvas.create_rectangle(x1, y1, x2, y2, fill=colorArray[i])
 
     root.update_idletasks()
 
 
-def StartAlgorithm():
-    global data
-    if not data:
-        return
-    if algo_menu.get() == 'Quick Sort':
-        quick_sort(data, 0, len(data) - 1, drawdata, speed_scale.get())
-        drawdata(data, ["green" for x in range(len(data))])
+# GENERATING ARRAY
+def createArr():
+    global arr
 
-    elif algo_menu.get() == 'Bubble Sort':
-        bubble_sort(data, drawdata, speed_scale.get())
+    array_size = 20
 
+    range_begin = 20
 
-def Generate():
-    global data
-    print("Selected Algorithm :" + selected_algorithm.get())
-    minivalue = int(minvalue.get())
+    range_end = 150
 
-    maxivalue = int(maxvalue.get())
+    arr = [122, 41, 134, 112, 41, 233, 32, 52, 123, 86, 212, 261]
+    for i in range(0, array_size):
+        random_integer = random.randint(range_begin,
+                                        range_end)  # starting from a higher value and not 0 because the vertical bars wont be visible
+        arr.append(random_integer)
 
-    sizeevalue = int(sizevalue.get())
-
-    data = []
-    for i in range(sizeevalue):
-        # We'll add that speed scaled by appending it
-        data.append(random.randrange(minivalue, maxivalue + 1))
-    drawdata(data, ['#A90042' for x in range(len(data))])
+    displayArr(arr, ["blue" for x in range(len(arr))])
 
 
-selected_algorithm = StringVar()
+# SORTING SPEED
 
-# Label,buttons
-mainlabel = Label(root, text="Algorithm : ", font=("new roman", 15, "italic bold"), bg="#05897A", width=10, fg="black",
-                  relief=GROOVE, bd=5)
-mainlabel.place(x=0, y=0)
+def set_speed():
+    slow = 0.8
+    medium = 0.35
+    fast = 0.05
 
-algo_menu = ttk.Combobox(root, width=15, font=("new roman", 18, "italic bold"), textvariable=selected_algorithm,
-                         values=["Bubble Sort", "Merge Sort", "Quick Sort"])
-algo_menu.place(x=145, y=0)
-algo_menu.current(0)  # by default bubble sort
+    if speed_comboBox.get() == 'Slow':
+        return slow
+    elif speed_comboBox.get() == 'Medium':
+        return medium
+    elif speed_comboBox.get() == "Fast":
+        return fast
 
-# Generate button
-random_generate = Button(root, text="Generate", bg="#2DAE9A", font=("arial", 12, "italic bold"), relief=SUNKEN,
-                         activebackground="#05945B", activeforeground="white", bd=5, width=10, command=Generate)
-random_generate.place(x=750, y=60)
+# SORTING ALGORIHMS !
+def sort():
+    tym = set_speed()
+    n = len(arr)
 
-# To choose specific size of array
-size_value_label = Label(root, text="Size:", font=("new roman", 12, "italic bold"), bg="#0E6DA5", width=10,
-                         fg="black", height=2, relief=GROOVE, bd=5)
-size_value_label.place(x=0, y=60)
+    if algo_comboBox.get() == 'Merge Sort':
+        merge_sort(arr, 0, len(arr) - 1, displayArr, tym)
 
-sizevalue = Scale(root, from_=0, to=30, resolution=1, orient=HORIZONTAL, font=("arial", 14, "italic bold"),
-                  relief=GROOVE, bd=2, width=10)
-sizevalue.place(x=120, y=60)
 
-# we'll set a particular range of numbers from min to max
-# min
-min_value_label = Label(root, text="Min Value:", font=("new roman", 12, "italic bold"), bg="#0E6DA5", width=10,
-                        fg="black", height=2, relief=GROOVE, bd=5)
-min_value_label.place(x=250, y=60)
+    elif algo_comboBox.get() == 'Selection Sort':
+        for i in range(0, n - 1):
+            for j in range(i + 1, n):
+                if (arr[i] > arr[j]):
+                    arr[i], arr[j] = arr[j], arr[i]  # SWAP
 
-minvalue = Scale(root, from_=0, to=10, resolution=1, orient=HORIZONTAL, font=("arial", 14, "italic bold"),
-                 relief=GROOVE, bd=2, width=10)
-minvalue.place(x=370, y=60)
+                    displayArr(arr, ["yellow" if x == j or x == j + 1 else "blue" for x in range(len(arr))])
+                    time.sleep(tym)
 
-# max
-max_value_label = Label(root, text="Max Value:", font=("new roman", 12, "italic bold"), bg="#0E6DA5", width=10,
-                        fg="black", height=2, relief=GROOVE, bd=5)
-max_value_label.place(x=500, y=60)
+        displayArr(arr, ["blue" for x in range(len(arr))])
 
-maxvalue = Scale(root, from_=0, to=100, resolution=1, orient=HORIZONTAL, font=("arial", 14, "italic bold"),
-                 relief=GROOVE, bd=2, width=10)
-maxvalue.place(x=620, y=60)
 
-# Start
-start = Button(root, text="Start", bg="#C45B09", font=("arial", 12, "italic bold"), relief=SUNKEN,
-               activebackground="#05945B", activeforeground="white", bd=5, width=10, command=StartAlgorithm)
-start.place(x=750, y=0)
 
-# Speed
-speed_label = Label(root, text="Speed: ", font=("new roman", 12, "italic bold"), bg="#0E6DA5", width=10,
-                    fg="black", relief=GROOVE, bd=5)
-speed_label.place(x=400, y=0)
+    elif algo_comboBox.get() == 'Bubble Sort':
+        for i in range(n - 1):
+            for j in range(0, n - i - 1):
+                if (arr[j] > arr[j + 1]):
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]  # SWAP
 
-speed_scale = Scale(root, from_=0.2, to=5.0, resolution=0.2, length=200, digits=2, orient=HORIZONTAL,
-                    font=("arial", 14, "italic bold"),
-                    relief=GROOVE, bd=2, width=10)
-speed_scale.place(x=520, y=0)
+                    displayArr(arr, ["yellow" if x == j else "red" if x == j + 1 else "blue" for x in range(len(arr))])
+                    time.sleep(tym)
 
-# bars window
-canvas = Canvas(root, width=870, height=450, bg="black")
-canvas.place(x=10, y=130)
+        displayArr(arr, ["blue" for x in range(len(arr))])
+
+
+
+    elif algo_comboBox.get() == 'Insertion Sort':
+        for i in range(1, len(arr)):
+
+            key = arr[i]
+            j = i - 1
+            while (j >= 0 and key < arr[j]):
+                arr[j + 1] = arr[j]
+                j -= 1
+
+                displayArr(arr, ["yellow" if x == j else "red" if x == j + 1 else "blue" for x in range(len(arr))])
+                time.sleep(tym)
+            arr[j + 1] = key
+
+        displayArr(arr, ["blue" for x in range(len(arr))])
+
+
+
+display_window = Frame(root, width=900, height=300, bg="white")
+display_window.grid(row=0, column=0, padx=10, pady=5)
+
+lbl1 = Label(display_window, text="Algorithm: ", bg="white")
+lbl1.grid(row=0, column=0, padx=10, pady=5, sticky=W)
+algo_comboBox = ttk.Combobox(display_window, textvariable=algo_name, values=algo_list)
+algo_comboBox.grid(row=0, column=1, padx=5, pady=5)
+algo_comboBox.current(0)
+
+lbl2 = Label(display_window, text="Sorting Speed: ", bg="white")
+lbl2.grid(row=1, column=0, padx=10, pady=5, sticky=W)
+speed_comboBox = ttk.Combobox(display_window, textvariable=speed_name, values=speed_list)
+speed_comboBox.grid(row=1, column=1, padx=5, pady=5)
+speed_comboBox.current(0)
+
+btn1 = Button(display_window, text="Sort", command=sort, bg="#C4C5BF")
+btn1.grid(row=4, column=1, padx=5, pady=5)
+
+btn2 = Button(display_window, text="Create Array", command=createArr, bg="#C4C5BF")
+btn2.grid(row=4, column=0, padx=5, pady=5)
+
+canvas = Canvas(root, width=800, height=400, bg="white")
+canvas.grid(row=1, column=0, padx=10, pady=5)
 
 root.mainloop()
